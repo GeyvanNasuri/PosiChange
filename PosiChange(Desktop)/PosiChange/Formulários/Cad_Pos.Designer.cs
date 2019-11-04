2d16eb7a6ed01a3368d6ed5391e71f83c42b012a
Gabriel Ribeiro <gabriel.ribeiro950@hotmail.com>
2019-11-03 23:32:04 -0300

    Carregar imagem no Windows Form

--- PosiChange(Desktop)/PosiChange/Formulários/Cad_Pos.Designer.cs
+++ PosiChange(Desktop)/PosiChange/Formulários/Cad_Pos.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.txt_posicao = new System.Windows.Forms.TextBox();
-            this.pic_posicao = new System.Windows.Forms.PictureBox();
-            this.btn_busca = new System.Windows.Forms.Button();
-            this.btn_salvar = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_salvar = new System.Windows.Forms.Button();
+            this.btn_busca = new System.Windows.Forms.Button();
+            this.pic_posicao = new System.Windows.Forms.PictureBox();
+            this.txt_posicao = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pic_posicao)).BeginInit();
             this.SuspendLayout();
@@ -56,52 +56,15 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Registrar Nova Posição";
             // 
-            // txt_posicao
+            // label2
             // 
-            this.txt_posicao.Location = new System.Drawing.Point(6, 40);
-            this.txt_posicao.Name = "txt_posicao";
-            this.txt_posicao.Size = new System.Drawing.Size(100, 20);
-            this.txt_posicao.TabIndex = 0;
-            // 
-            // pic_posicao
-            // 
-            this.pic_posicao.BackColor = System.Drawing.Color.White;
-            this.pic_posicao.Location = new System.Drawing.Point(6, 97);
-            this.pic_posicao.Name = "pic_posicao";
-            this.pic_posicao.Size = new System.Drawing.Size(243, 222);
-            this.pic_posicao.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pic_posicao.TabIndex = 1;
-            this.pic_posicao.TabStop = false;
-            // 
-            // btn_busca
-            // 
-            this.btn_busca.Location = new System.Drawing.Point(255, 97);
-            this.btn_busca.Name = "btn_busca";
-            this.btn_busca.Size = new System.Drawing.Size(75, 23);
-            this.btn_busca.TabIndex = 2;
-            this.btn_busca.Text = "Buscar";
-            this.btn_busca.UseVisualStyleBackColor = true;
-            this.btn_busca.Click += new System.EventHandler(this.btn_busca_Click);
-            // 
-            // btn_salvar
-            // 
-            this.btn_salvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_salvar.Location = new System.Drawing.Point(6, 325);
-            this.btn_salvar.Name = "btn_salvar";
-            this.btn_salvar.Size = new System.Drawing.Size(109, 23);
-            this.btn_salvar.TabIndex = 3;
-            this.btn_salvar.Text = "Salvar Informações";
-            this.btn_salvar.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(121, 325);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(128, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Voltar à Página Pricipal";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Imagem";
             // 
             // label1
             // 
@@ -113,15 +76,52 @@
             this.label1.TabIndex = 5;
             this.label1.Text = "Posição";
             // 
-            // label2
+            // button1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 77);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 17);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Imagem";
+            this.button1.Location = new System.Drawing.Point(121, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Voltar à Página Pricipal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_salvar
+            // 
+            this.btn_salvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salvar.Location = new System.Drawing.Point(6, 325);
+            this.btn_salvar.Name = "btn_salvar";
+            this.btn_salvar.Size = new System.Drawing.Size(109, 23);
+            this.btn_salvar.TabIndex = 3;
+            this.btn_salvar.Text = "Salvar Informações";
+            this.btn_salvar.UseVisualStyleBackColor = true;
+            // 
+            // btn_busca
+            // 
+            this.btn_busca.Location = new System.Drawing.Point(255, 97);
+            this.btn_busca.Name = "btn_busca";
+            this.btn_busca.Size = new System.Drawing.Size(75, 23);
+            this.btn_busca.TabIndex = 2;
+            this.btn_busca.Text = "Buscar";
+            this.btn_busca.UseVisualStyleBackColor = true;
+            this.btn_busca.Click += new System.EventHandler(this.btn_busca_Click);
+            // 
+            // pic_posicao
+            // 
+            this.pic_posicao.BackColor = System.Drawing.Color.White;
+            this.pic_posicao.Location = new System.Drawing.Point(6, 97);
+            this.pic_posicao.Name = "pic_posicao";
+            this.pic_posicao.Size = new System.Drawing.Size(243, 222);
+            this.pic_posicao.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_posicao.TabIndex = 1;
+            this.pic_posicao.TabStop = false;
+            // 
+            // txt_posicao
+            // 
+            this.txt_posicao.Location = new System.Drawing.Point(6, 40);
+            this.txt_posicao.Name = "txt_posicao";
+            this.txt_posicao.Size = new System.Drawing.Size(100, 20);
+            this.txt_posicao.TabIndex = 0;
             // 
             // Cad_Pos
             // 
